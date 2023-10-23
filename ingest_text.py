5e3533a5846b8bef718092966e7da3fe4c7dc4b4
sashaTribe <sasha555.tribe@gmail.com>
2023-10-23 15:38:55 +0100

    created my ingest file, imported libraries needed

---
+++ ingest_text.py
@@ -0,0 +1,7 @@
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import pinecone
+
+import os
+
