6de83a39e1a0ebdafc3a1aaee7dea535102f5a86
sashaTribe <sasha555.tribe@gmail.com>
2023-10-23 15:45:09 +0100

    downloaded pinecone packages and imported it too

--- ingest_text.py
+++ ingest_text.py
@@ -2,6 +2,6 @@ from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import pinecone
-
+import pinecone
 import os
 
