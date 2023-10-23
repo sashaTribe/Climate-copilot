9151f025b70532339aff4ee41ca8c50f0eece959
sashaTribe <sasha555.tribe@gmail.com>
2023-10-23 16:22:05 +0100

    just imported bs4

--- ingest_text.py
+++ ingest_text.py
@@ -1,7 +1,10 @@
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import AsyncHtmlLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import pinecone
+from langchain.chat_models import ChatOpenAI
+from bs4 import BeautifulSoup
+import pprint
 import pinecone
 import os
 
@@ -11,3 +14,11 @@ if os.path.exists("env.py"):
 pinecone.init(api_key=os.environ("PINECONE_SECRET_KEY"),
               environment=os.environ.get("PINECONE_ENVIRONMENT_REGION"))
 
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+url = "https://www.theccc.org.uk/publications/"
+loader = AsyncHtmlLoader(url)
+docs = loader.load()
+
+
+def grab_corpus(url,schema):
+    loader = Async
