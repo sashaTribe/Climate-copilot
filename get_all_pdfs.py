64b0c0f2bb8f8e9adc5374157d5df609c1ae1b60
sashaTribe <sasha555.tribe@gmail.com>
2023-10-25 20:54:12 +0100

    Got all pdf links ready

--- get_all_pdfs.py
+++ get_all_pdfs.py
@@ -10,54 +10,75 @@ def create_web_reader(url):
     return html_content
 
 def all_report_links(url):
-    read = requests.get(url)
-    html_content = read.content
+    html_content = create_web_reader(url)
     soup = BeautifulSoup(html_content, "html.parser")
     list_of_links = []
-    links = soup.find_all('a', {'class': 'wp-block-button__link'})
+    links = soup.find_all('a', {'class': 'post-tile__link'})
     for link in links:
         temp = link.get("href")
         list_of_links.append(temp)
     return list_of_links
+#list = all_report_links(url)
+#print(list[0])
 
-print(all_report_links(url))
-
-def get_report_link(given_link):
-    read = requests.get(given_link)
-    html_content = read.content
-    soup = BeautifulSoup(html_content, "html.parser")
-    links = soup.find('a').get('href')
-    links = soup.find_all('a', {'class': 'post-title__link'})
+def get_report_links():
     list_of_links = []
-    for link in links:
-        temp = link.get("href")
-        list_of_links.append(temp)
+    for i in range(1,34):
+        list_of_links.append(f'https://www.theccc.org.uk/publications/page/{i}/?topic&type=0-report')
     return list_of_links
+
+print(len(get_report_links()))
+"""
+lst = []
+for link in all_report_links(url):
+    print(get_report_link(link))
+    print('\n')
+"""
+
 
 def get_pdf(url):
+    html_content = create_web_reader(url)
+    soup = BeautifulSoup(html_content,"html.parser")
+    a_tags = soup.find_all('a')
+    for a_tag in a_tags:
+        link = a_tag.get('href')
+        if '.pdf' in link:
+            return link
 
-    read = requests.get(url)
-    html_content = read.content
-    soup = BeautifulSoup(html_content, "html.parser")
-    link = soup.find_all('a', {'class': 'wp-block-button_link is-align-left'})
-    return link
+def put_together():
+    list_of_links = get_report_links()
+    pdf_links = []
+    for link in list_of_links:
+        temp_list = all_report_links(link)
+        for final_link in temp_list:
+            pdf = get_pdf(final_link)
+            #print(pdf)
+            if pdf != None:
+                pdf_links.append(pdf)
+    return pdf_links
+"""
+ test_list, test_list_two = put_together(url)
+#my_list = [x for x in test_list if x != None]
+my_list_two = [x for x in test_list_two if x != None]
+#print(len(my_list))
+print(len(my_list_two))
+print(my_list_two[0])
+ """   
 
 
-    
 def put_pdfs_in_file(pdf_list):
     with open('pdf_list.txt', 'w') as f:
         for pdf in pdf_list:
             if pdf != '.pdf':
                 f.write(pdf)
                 f.write('\n')
-        
 
 
 def main():
-    list_of_links = get_tag_a('https://www.theccc.org.uk/publications/')
-    for link in list_of_links:
-        temp_list = (get_pdf(link))
-        put_pdfs_in_file(temp_list)
+    pdf_links = put_together()
+    put_pdfs_in_file(pdf_links)
+    print(len(pdf_links))
 
-#main()
+
+main()
 
--- grab_text.py
+++ grab_text.py
@@ -8,7 +8,10 @@ def extract_text(pdf_link):
     response = requests.get(pdf_link)
     f = io.BytesIO(response.content)
     reader = PyPDF2.PdfReader(f)
-    
-    print(reader.pages[2].extract_text().split('\n'))
+    num_pages = len(reader.pages)
+    for page_number in range(num_pages):
+        page = reader.pages[page_number]
+        page_text = page.extract_text()
+        print(f"Page {page_number + 1}:\n{page_text}\n")
 
-extract_text("https://www.theccc.org.uk/wp-content/uploads/2023/09/230925-PF-MN-ZEV-Mandate-Response.pdf")
+extract_text("https://www.theccc.org.uk/wp-content/uploads/2021/10/Independent-Assessment-of-the-UK-Net-Zero-Strategy-CCC.pdf")
