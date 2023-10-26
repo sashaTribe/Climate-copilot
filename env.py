0f55956241ee3563c3928876d6e23832679c2fa8
sashaTribe <sasha555.tribe@gmail.com>
2023-10-27 00:55:17 +0100

    added documentation for main

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
 import os 
 
+# List of API keys below
 os.environ.setdefault("OPENAI_API_KEY", "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")
 os.environ.setdefault("PINECONE_SECRET_KEY", "75889be0-ca5d-4da5-a3d2-8abddad30441")
 os.environ.setdefault("PINECONE_ENVIRONMENT_REGION", "gcp-starter")
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 if os.path.exists("env.py"):
     import env
 
+# Runs the whole program
 if __name__ == "__main__":
     run_llm(os.environ.get("PINECONE_ENVIRONMENT_REGION"))
 
