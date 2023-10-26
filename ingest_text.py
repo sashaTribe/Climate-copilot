18a752e4b537c1b12ca9297484181d034931e0a2
sashaTribe <sasha555.tribe@gmail.com>
2023-10-27 00:54:14 +0100

    adds documentation to the file

--- ingest_text.py
+++ ingest_text.py
@@ -9,11 +9,22 @@ from langchain.vectorstores import Pinecone
 if os.path.exists("env.py"):
     import env
 
+# initialises pinecone client 
 pinecone.init(api_key=os.environ.get("PINECONE_SECRET_KEY"),
               environment=os.environ.get("PINECONE_ENVIRONMENT_REGION"))
 active_indexes = pinecone.list_indexes()
+# gets index of the client you are submitting embeddings to
 index = pinecone.Index('climate-change')
 
+"""
+reads all the pdf links from the given file
+
+parameters:
+- pdf_list_file --> the file name of all the pdf links stored
+
+return:
+- pdf_link_list --> a list containing string objects (pdf web links)
+"""
 def get_all_pdfs(pdf_list_file):
     pdf_link_list = []
     with open(pdf_list_file, 'r') as f:
@@ -23,7 +34,15 @@ def get_all_pdfs(pdf_list_file):
     return pdf_link_list
 
 
+"""
+Extracts text data from the given pdf 
 
+parameters:
+- pdf_path --> type str containing the path of the pdf
+
+returns:
+- text --> a big string of text 
+"""
 def extract_text_from_pdf(pdf_path):
     text = ""
     with PyPDF2.open(pdf_path) as pdf:
@@ -31,12 +50,22 @@ def extract_text_from_pdf(pdf_path):
             text += page.extract_text()
     return text
 
+"""
+loads and splits data using Langchain
+parameters:
+- pdf --> str type
+- pages --> a list of string objects
+"""
 def load_pdf(pdf):
     print(pdf)
     loader = PyPDFLoader(pdf)
     pages = loader.load_and_split()
     return pages
 
+"""
+extracts text from given pages
+- page --> list of string objects
+"""
 def extract_text(page):
     text = ""
     text_splitter = RecursiveCharacterTextSplitter(
@@ -44,16 +73,22 @@ def extract_text(page):
         chunk_overlap = 50,
         separators =["\n\n", "\n", "",""]
     )
+    # splits the pages up into smaller texts
     document = text_splitter.split_documents(page)
+    # creates embedding tool from openai 
     embedding = OpenAIEmbeddings(open_ai_key = os.environ.get("OPEN_AI_KEY"))
+    # processes the documents, creating embeddings for each document and upload them to
+    # pinecone client called 'climate-change'
     Pinecone.from_documents(documents = document, embedding=embedding, index_name='climate-change')
     print("Successful upload")
 
+# This calls all the functions above
 def upload_pdf():
     path = 'pdfs/'
     #pdf_files = [f for f in os.listdir(path) if f.endswith('.pdf')]
     pdf_files = []
     for file in os.listdir(path):
+        # joining the path and file together 
         if os.path.isfile(os.path.join(path, file)):
             pdf_files.append(os.path.join(path, file))
     print(len(pdf_files))
