4a16c77ee9eddb577ba4590a26f038fb532a0563
sashaTribe <sasha555.tribe@gmail.com>
2023-10-26 14:16:32 +0100

    made changes to env and grab-text file
    added functions

--- env.py
+++ env.py
@@ -1,4 +1,6 @@
 import os 
 
-os.environ.setdefault("OPENAI_API_KEY", "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")
+os.environ.setdefault("OPENAI_API_KEY", "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")
 os.environ.setdefault("PINECONE_SECRET_KEY", "75889be0-ca5d-4da5-a3d2-8abddad30441")
+os.environ.setdefault("PINECONE_ENVIRONMENT_REGION", "gcp-starter")
+os.environ.setdefault("index_name","climate-change")
--- grab_text.py
+++ grab_text.py
@@ -1,17 +1,82 @@
 import requests
-import bs4
-import csv
 import PyPDF2
 import io 
+import os
+output_folder = "pdfs/"
+import fitz
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pinecone
+from langchain.vectorstores import Pinecone
 
-def extract_text(pdf_link):
-    response = requests.get(pdf_link)
-    f = io.BytesIO(response.content)
-    reader = PyPDF2.PdfReader(f)
-    num_pages = len(reader.pages)
-    for page_number in range(num_pages):
-        page = reader.pages[page_number]
-        page_text = page.extract_text()
-        print(f"Page {page_number + 1}:\n{page_text}\n")
+if os.path.exists("env.py"):
+    import env
+pinecone.init(api_key=os.environ.get("PINECONE_SECRET_KEY"),
+              environment=os.environ.get("PINECONE_ENVIRONMENT_REGION"))
+active_indexes = pinecone.list_indexes()
+index = pinecone.Index('climate-change')
 
-extract_text("https://www.theccc.org.uk/wp-content/uploads/2021/10/Independent-Assessment-of-the-UK-Net-Zero-Strategy-CCC.pdf")
+def get_all_pdfs(pdf_list_file):
+    pdf_link_list = []
+    with open(pdf_list_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            pdf_link_list.append(line)
+    return pdf_link_list
+
+
+def load_pdf(pdf):
+    loader = PyPDFLoader(pdf)
+    pages = loader.load_and_split()
+    return pages
+
+def extract_text(page):
+    text = ""
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 250,
+        chunk_overlap = 50,
+        separators =["\n\n", "\n", "",""]
+    )
+    document = text_splitter.split_documents(page)
+    embedding = OpenAIEmbeddings(open_ai_key = os.environ.get("OPEN_AI_KEY"))
+    Pinecone.from_documents(documents = document, embedding=embedding, index_name='climate-change')
+    print("Successful upload")
+
+
+text = load_pdf("https://www.theccc.org.uk/wp-content/uploads/2023/09/230925-PF-MN-ZEV-Mandate-Response.pdf")
+extract_text(text)
+
+
+
+
+def load_pdfs_to_loader(pdf_list):
+    list_of_pages = []
+
+    for pdf in pdf_list:
+        list_of_pages.append(load_pdf(pdf))
+    for pages in list_of_pages:
+        faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
+        docs = faiss_index.similarity_search("How will the community be engaged?", k=2)
+
+    for doc in docs:
+        print(str(doc.metadata["page"]) + ":", doc.page_content[:300])
+
+
+def main():
+    #list_of_pdf_links = get_all_pdfs('pdf_list.txt')
+    #load_pdfs_to_loader(list_of_pdf_links)
+    page = load_pdf("https://www.theccc.org.uk/wp-content/uploads/2023/09/230925-PF-MN-ZEV-Mandate-Response.pdf")
+    return len(page)
+    
+    faiss_index = FAISS.from_documents(page, OpenAIEmbeddings())
+    docs = faiss_index.similarity_search("Climate? ", k=2)
+    for doc in docs:
+        print(str(doc.metadata["page"]) + ":", doc.page_content[:300])
+
+    
+
+
+#extract_text("https://www.theccc.org.uk/wp-content/uploads/2021/10/Independent-Assessment-of-the-UK-Net-Zero-Strategy-CCC.pdf")
+
