bc7eff920c5acd3a830889e557ca14943eed9640
sashaTribe <53623386+sashaTribe@users.noreply.github.com>
2023-11-09 19:24:54 +0000

    Update env.py
    removed my API keys

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os 
 
 # List of API keys below
-os.environ.setdefault("OPENAI_API_KEY", "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")
-os.environ.setdefault("PINECONE_SECRET_KEY", "75889be0-ca5d-4da5-a3d2-8abddad30441")
-os.environ.setdefault("PINECONE_ENVIRONMENT_REGION", "gcp-starter")
+os.environ.setdefault("OPENAI_API_KEY", "#")
+os.environ.setdefault("PINECONE_SECRET_KEY", "#")
+os.environ.setdefault("PINECONE_ENVIRONMENT_REGION", "#")
 os.environ.setdefault("index_name","climate-change")
