a6490e4622e4f196943edf781ea2f3b70b4f440d
sashaTribe <sasha555.tribe@gmail.com>
2024-04-24 18:27:57 +0100

    ignore commit

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("OPENAI_API_KEY", "sk-proj-UDXm6xuOcJDIjUkjCFV1T3BlbkFJfs6OGb4txW7PI4a4LHVE")
+os.environ.setdefault("PINECONE_SECRET_KEY", "75889be0-ca5d-4da5-a3d2-8abddad30441")
+os.environ.setdefault("PINECONE_ENVIRONMENT_REGION", "us-central1")
+os.environ.setdefault("index_name","climate-change")
